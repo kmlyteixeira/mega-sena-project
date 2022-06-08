c3367223c01f76767955ff72e380f5dc592911a6
Kemily Teixeira <101020416+kmlyteixeira@users.noreply.github.com>
2022-06-08 20:37:44 -0300

    FinalUpdate

--- jogoPrincipal.cpp
+++ jogoPrincipal.cpp
@@ -38,10 +38,31 @@ main()
         spins = 0;
         cout << "\n\n\tOpcao invalida! Escolha um numero de 6 a 15 >>> ";
     } else {
-        x = 1;  
+        x = 1; 
+		if(spins == 6)
+			cout << "\n\n\tVoce vai pagar R$4,50!";	
+		if(spins == 7)
+			cout << "\n\n\tVoce vai pagar R$31,50!";	
+		if(spins == 8)
+			cout << "\n\n\tVoce vai pagar R$126,00!";	
+		if(spins == 9)
+			cout << "\n\n\tVoce vai pagar R$378,00!";	
+		if(spins == 10)
+			cout << "\n\n\tVoce vai pagar R$945,00!";	
+		if(spins == 11)
+			cout << "\n\n\tVoce vai pagar R$2.079,00!";	
+		if(spins == 12)
+			cout << "\n\n\tVoce vai pagar R$4.158,00!";	
+		if(spins == 13)
+			cout << "\n\n\tVoce vai pagar R$7.722,00!";	
+		if(spins == 14)
+			cout << "\n\n\tVoce vai pagar R$13.513,50!";	
+		if(spins == 15)
+			cout << "\n\n\tVoce vai pagar R$22.522,50!";	 
     }
     } while (x == 0);
-
+	
+	sleep(1);
     cout << "\n\n\tGostaria de informar seus numeros da sorte ou deseja rodar aleatoriamente?";
     cout << "\n\t[S] NUMERO DA SORTE | [A] ALEATORIZAR JOGADAS \n\t";
     do{
@@ -56,9 +77,24 @@ main()
         cout << "\n\n\tVamos la! Voce devera informar "<<aux<<" numeros entre 1 - 60";      
         for(int i=0; i<aux; i++){
             cout << "\n\n\tNumero "<<i+1<<" da aposta >>>";
-			cin >> number[i];
+			x=0;
+			do{
+				cin >> number[i];
+				auxiliar[i] = number[i];
+				if(i>=1){
+					for(int j=0; j<=(i-1); j++){
+						if ((auxiliar[j] == number[i])||((number[i]>60)||(number[i]<1))){
+							cout << "\n\n\tNumero invalido. Tente novamente >>>";	
+							i = i-1;
+						} else {
+							x=1;
+						}
+					}	
+				}
+			} while(x=0);	
         }
         
+        cout << "\n\tSEUS NUMEROS ESCOLHIDOS >>> ";
         bubble_sort_cresc(number,aux);
         cout << "\n\n\t";
         for(int i=0; i<aux; i++){
@@ -122,9 +158,20 @@ main()
     
     srand((unsigned)time(NULL));
 	for (int i = 0 ; i < 6; i++){
-	    sort[i] = ((rand()%60)/2)-3;
+	    sort[i] = ((rand()%58)+2)-3;
 	}
-	cout << "\n\n\tRESULTADO DA MEGA SENA: ";
+	
+	sleep(1);
+	cout << "\n\n\tO RESULTADO DA MEGA SENA SAI EM... ";
+	sleep(1);
+	cout << "3... ";
+	sleep(1);
+	cout << "2... ";
+	sleep(1);
+	cout << "1... ";
+	sleep(1);
+	
+	cout << "\n\n\tNUMEROS SORTEADOS >>> ";
 	for (int i = 0 ; i < 6; i++){
 	    cout << " | " << sort[i];
 	}
@@ -136,12 +183,14 @@ main()
 			}	
 		}	
 	}
+	
+	sleep(2);
     if (cont<4){
-    	cout << "\n\n\tMa noticia. Nao foi desta vez! Quantidade de acertos >>> " << cont;
+    	cout << "\n\n\tMa noticia. Nao foi desta vez, mas continue tentando! Quantidade de acertos >>> " << cont;
 	} else if (cont==4){
-		cout << "\n\n\tVOCÊ GANHOU um PEQUENO valor! Quantidade de acertos >>> " << cont << " | SEU PREMIO: R$923,68";
+		cout << "\n\n\tVOCÊ GANHOU um PEQUENO valor! Quantidade de acertos >>> " << cont << " | SEU lindo PREMIO: R$923,68";
 	} else if (cont==5){
-		cout << "\n\n\tVOCÊ GANHOU um valor CONSIDERÁVEL! Quantidade de acertos >>> " << cont << " | SEU PREMIO: R$46.388,86";
+		cout << "\n\n\tVOCÊ GANHOU um valor CONSIDERÁVEL! Quantidade de acertos >>> " << cont << " | SEU lindo PREMIO: R$46.388,86";
 	} else if (cont==6){
 		cout << "\n\n\tVOCÊ FOI O GRANDE VENCEDOR! ! ! Acertou a SENA!";
 	}
